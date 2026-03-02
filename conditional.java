5fcd9e20bb0e9a010af3e5442a2e2ff7eca9dd3d
muskankashyap-spice <muskan106kashyap@gmail.com>
2026-03-02 20:37:55 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,276 @@
+import java.util.Scanner;
+public class conditional {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
+public static void main(String[]args){
+          Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+  public static void main(String[]args){
+          Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+    /*---------------------------------------------- */
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        int n= sc.nextInt();
+        if(n>0){
+            System.out.println("Positive");
+        }else if(n<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        int n= sc.nextInt();
+        if(n>0){
+            System.out.println("Positive");
+        }else if(n<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        int n= sc.nextInt();
+        if(n>0){
+            System.out.println("Positive");
+        }else if(n<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
+    }
+    /*----------------------------------------- */
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int marks=sc.nextInt();
+        if(marks>=40){
+            System.out.println("Pass");
+        }else{
+        System.out.println("Fail");
+        }
+    }
+     public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int marks=sc.nextInt();
+        if(marks>=40){
+            System.out.println("Pass");
+        }else{
+        System.out.println("Fail");
+        }
+    }
+     public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int marks=sc.nextInt();
+        if(marks>=40){
+            System.out.println("Pass");
+        }else{
+        System.out.println("Fail");
+        }
+    }
+    /*---------------------------------------------- */
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         if(a > b){
+              System.out.println(a);
+         }else{
+               System.out.println(b);
+          }
+    }
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         if(a > b){
+              System.out.println(a);
+         }else{
+               System.out.println(b);
+          }
+    }
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         if(a > b){
+              System.out.println(a);
+         }else{
+               System.out.println(b);
+          }
+    }
+    /*--------------------------------------- */
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          int age = sc.nextInt();
+          if(age >= 18){
+              System.out.println("Eligible");
+           } else{
+               System.out.println("Not Eligible");
+           }
+    }
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          int age = sc.nextInt();
+          if(age >= 18){
+              System.out.println("Eligible");
+           } else{
+               System.out.println("Not Eligible");
+           }
+    }
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          int age = sc.nextInt();
+          if(age >= 18){
+              System.out.println("Eligible");
+           } else{
+               System.out.println("Not Eligible");
+           }
+    }
+    /*---------------------------------------------- */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        if(n % 5 == 0){
+               System.out.println("Divisible");
+         } else{
+               System.out.println("Not Divisible");
+         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        if(n % 5 == 0){
+               System.out.println("Divisible");
+         } else{
+               System.out.println("Not Divisible");
+         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        if(n % 5 == 0){
+               System.out.println("Divisible");
+         } else{
+               System.out.println("Not Divisible");
+         }
+    }
+    /*----------------------------------------- */
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+         int temp = sc.nextInt();
+        if(temp > 25){
+             System.out.println("Hot");
+        }else{
+             System.out.println("Cool");
+         }
+    }
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+         int temp = sc.nextInt();
+        if(temp > 25){
+             System.out.println("Hot");
+        }else{
+             System.out.println("Cool");
+         }
+    }
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+         int temp = sc.nextInt();
+        if(temp > 25){
+             System.out.println("Hot");
+        }else{
+             System.out.println("Cool");
+         }
+    }
+    /*----------------------------------- */
+   
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int year = sc.nextInt();
+      if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+     System.out.println("Leap Year");
+    }else{
+      System.out.println("Not Leap Year");
+      }
+  }
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int year = sc.nextInt();
+      if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+     System.out.println("Leap Year");
+    }else{
+      System.out.println("Not Leap Year");
+      }
+  }
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int year = sc.nextInt();
+      if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+     System.out.println("Leap Year");
+    }else{
+      System.out.println("Not Leap Year");
+      }
+  }
+  /*------------------------------------------- */
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+     if(a + b > c && a + c > b && b + c > a){
+       System.out.println("Valid Triangle");
+     }else{
+        System.out.println("Invalid Triangle");
+    }
+}
+public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+     if(a + b > c && a + c > b && b + c > a){
+       System.out.println("Valid Triangle");
+     }else{
+        System.out.println("Invalid Triangle");
+    }
+}
+public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+     if(a + b > c && a + c > b && b + c > a){
+       System.out.println("Valid Triangle");
+     }else{
+        System.out.println("Invalid Triangle");
+    }
+}
